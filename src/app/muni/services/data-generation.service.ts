28c157d64ca3862b2d34ec91ac5902f379e20254
abel <soyabelca@gmail.com>
2024-04-24 09:46:59 -0500

    update captcha

--- src/app/muni/services/data-generation.service.ts
+++ src/app/muni/services/data-generation.service.ts
@@ -8,7 +8,7 @@ export class DataGenerationService {
   constructor() { }
   textGenerator(): string {
     let generatedText = '';
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 2; i++) {
       generatedText += String.fromCharCode(this.randomNumber(65, 90));
       generatedText += String.fromCharCode(this.randomNumber(48, 57));
     }
